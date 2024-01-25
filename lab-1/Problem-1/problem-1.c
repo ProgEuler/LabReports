9fb8867c3fc359b4e6b5b26aed7ed892cabbe015
ProgEuler <sarufkhan568@gmail.com>
2024-01-25 16:44:25 +0600

    Solve

---
+++ lab-1/Problem-1/problem-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   
+    char name[] = "Alexandra Abramov";
+    char dob[] = "July 14, 1975";
+    char mobile[] = "99-9999999999";
+
+    printf("Name   : %s\n", name);
+    printf("DOB    : %s\n", dob);
+    printf("Mobile : %s\n", mobile);
+
+    return 0;
+}
