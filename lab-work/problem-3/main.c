a6b3496cb49162617c8c29bbcb3842f4bd9dc5db
ProgEuler <sarufkhan568@gmail.com>
2024-02-18 19:21:00 +0600

    solve

---
+++ lab-work/problem-3/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char grade;
+    printf("Enter your grade : ");
+    scanf("%c", &grade);
+
+    switch(grade){
+    	case 'E' :
+    		printf("You have chosen Excellent\n");
+    	break;
+    	case 'V' :
+    		printf("You have chosen Very Good\n");
+    	break;
+    	case 'G' :
+    		printf("You have chosen Good\n");
+    	break;
+    	case 'A' :
+    		printf("You have chosen Average\n");
+    	break;
+    	case 'F' :
+    		printf("You have chosen Fail\n");
+    	break;
+    }
+    return 0;
+}
