692ae2fd8035653e75a51513b6bf3fd2afd4155b
ProgEuler <sarufkhan568@gmail.com>
2024-02-18 16:58:54 +0600

    solve

---
+++ lab-work/problem-1/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    char input;
+    printf("Enter your alphabet : ");
+    scanf("%c", &input);
+
+    switch(input){
+    	case 'A' :
+    	case 'a' :
+    		printf("Its a vowel.\n");
+    		break;
+    	case 'O' :
+    	case 'o' :
+    		printf("Its a vowel.\n");
+    		break;
+    	case 'I' :
+    	case 'i' :
+    		printf("Its a vowel.\n");
+    		break;
+    	case 'E' :
+    	case 'e' :
+    		printf("Its a vowel.\n");
+    		break;
+    	case 'U' :
+    	case 'u' :
+    		printf("Its a vowel.\n");
+    		break;
+    	default :
+    		printf("Its a consonant.\n");
+    }
+    return 0;
+}
